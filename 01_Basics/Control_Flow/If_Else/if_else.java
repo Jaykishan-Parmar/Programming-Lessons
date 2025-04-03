2aa470731be412b07452a4bf7014906837170fb3
SachinRajyaguru <rajyagurusachin1999@gmail.com>
2025-04-04 01:46:19 +0530

    add if else for java

---
+++ 01_Basics/Control_Flow/If_Else/if_else.java
@@ -0,0 +1,9 @@
+class if_else {
+    public static void main(String a[]){
+        if(5 >= 6) {
+            System.out.println("sachu che bhai");
+        } else {
+            System.out.println("budhi chod");
+        }
+    }
+}
